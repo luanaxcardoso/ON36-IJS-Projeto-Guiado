fd5b8fdd2cb8b304ee731afaa90abc7ed5f34122
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-08-18 23:53:33 -0300

    feat(aluno): implementar verificação de duplicidade e idade mínima no cadastro de aluno

--- src/aluno/aluno.service.ts
+++ src/aluno/aluno.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException, ConflictException } from '@nestjs/common';
 import { CreateAlunoDto } from './dto/create-aluno.dto';
 import { Aluno } from './entities/aluno.entity';
 
@@ -8,6 +8,21 @@ export class AlunoService {
   private readonly alunos: Aluno[] = [];
 
   cadastrar(createAlunoDto: CreateAlunoDto): Aluno {
+    
+    const alunoExistente = this.alunos.find(aluno => aluno.nome === createAlunoDto.nome);
+    if (alunoExistente) {
+      throw new ConflictException('Já existe um aluno com esse nome.');
+    }
+
+    
+    const anoAtual = new Date().getFullYear();
+    const idade = anoAtual - createAlunoDto.anoNascimento;
+    const IDADE_MIN_CADASTRO = 16;
+
+    if (idade <= IDADE_MIN_CADASTRO) {
+      throw new ForbiddenException('A idade mínima para cadastro é 17 anos.');
+    }
+    
     const novoAluno: Aluno = {
       id: this.alunos.length + 1,
       ...createAlunoDto,
