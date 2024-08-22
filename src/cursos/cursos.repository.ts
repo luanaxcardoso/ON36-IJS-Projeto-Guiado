4b177c13a4bafd910ceb2b787ae7b1ea3ec6ef6e
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-08-22 11:47:10 -0300

    feat: adiciona repositórios para Aluno e Curso
    - Adicionado o repositório AlunoRepository para gerenciamento de dados de alunos.
    - Adicionado o repositório CursoRepository para gerenciamento de dados de cursos.

---
+++ src/cursos/cursos.repository.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { Curso } from './entities/curso.entity';
+
+@Injectable()
+export class CursosRepository {
+  private cursos: Curso[] = [];
+
+  criar(curso: Curso): Curso {
+    this.cursos.push(curso);
+    return curso;
+  }
+
+  listar(): Curso[] {
+    return this.cursos;
+  }
+
+  buscarPorId(id: string): Curso | undefined {
+    return this.cursos.find(curso => curso.id === id);
+  }
+
+  salvar(curso: Curso): Curso {
+    const index = this.cursos.findIndex((c) => c.id === curso.id);
+    if (index !== -1) {
+      this.cursos[index] = curso;
+      return curso;
+    }
+    throw new Error('Curso não encontrado');
+  }
+}
